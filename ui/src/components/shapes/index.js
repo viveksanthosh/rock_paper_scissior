2f876b6fd44ffe1d10b7e33d9c8515d70ceabe11
Vivek <vivek.santhosh@in.tesco.com>
2017-08-12 17:15:24 +0530

    added the shape components

---
+++ ui/src/components/shapes/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './shapes.css';
+
+export const Circle = () => (<div className="circle"></div>);
+export const Square = () => (<div className="square"></div>);
+export const X = () => (<div className="orangeBox"><span >X</span></div>); 
