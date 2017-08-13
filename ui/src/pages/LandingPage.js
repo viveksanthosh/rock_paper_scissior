15712ccab72fbf8387c344c795f4d7d104bb932f
Vivek <vivek.santhosh@in.tesco.com>
2017-08-13 19:56:30 +0530

    added ui tweaks

--- ui/src/pages/LandingPage.js
+++ ui/src/pages/LandingPage.js
@@ -12,11 +12,17 @@ export default ({ userCount, lookingForOpponent }) => (
                 <br />
                 <p className="largeWhite">{userCount}</p>
                 <p className="largeWhite">Player currently online</p>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <div style={{ textAlign: "center" }}>
                     <button className="btn btn-primary" onClick={lookingForOpponent}> PLAY NOW</button>
                 </div>
+                <br />
+                <br />
+                <p className="largeWhite" style={{ textDecoration: "underline" }}>RULES</p>
+                <p className="normalWhite" >1. The Circle is rock, square is paper and the X is scissor</p>
+                <p className="normalWhite" >2. Rock beats scissor, scissor beats paper and paper beats rock</p>
+                <p className="normalWhite" >3. You have 5 seconds to choose, if you dont choose and your opponent does, you lose !</p>
             </div>
         </div>
     </div>
